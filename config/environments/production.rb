af6662923fe709adeb41a67d22a0803a6f074298
adam nelson <mybigf@gmail.com>
2013-08-18 13:27:52 +1000

    Amazon US stardard config - take 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Omrails::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
 	:path =&gt; ":class/:id/:basename_:style.:extension",
 	:url =&gt; ":s3_sg_url"
   }
